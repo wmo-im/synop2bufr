67066432ca85b784791c6f84d0adb6f5b9c80fac
Tom Kralidis <tomkralidis@gmail.com>
2023-03-12 18:48:06 -0400

    back to dev

--- synop2bufr/__init__.py
+++ synop2bufr/__init__.py
@@ -33,7 +33,7 @@ from typing import Iterator, Tuple
 from csv2bufr import BUFRMessage
 from pymetdecoder import synop
 
-__version__ = '0.2.0'
+__version__ = '0.3.dev0'
 
 LOGGER = logging.getLogger(__name__)
 
